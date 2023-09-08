0c0eb9ab2d54113a0db8b0ef56adede69a54c6e8
Alireza zamani <zamani802000@gmail.com>
2023-09-08 16:35:27 +0330

    fix domain

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,15 +4,13 @@ export async function POST(request: NextRequest) {
   const res = await request.json();
 
   const headers = new Headers();
-  console.log(res?.at, res?.rt);
-
   headers.append(
     "Set-Cookie",
-    `x-access-token=${res?.at}; Path=/; HttpOnly;  Max-Age=64000000 Domain=.webidemyyy.ir  SameSite=Lax;`
+    `x-access-token=${res?.at}; Path=/; Domain=.webidemyyy.ir; HttpOnly;  Max-Age=64000000 SameSite=Lax;`
   );
   headers.append(
     "Set-Cookie",
-    `x-refresh-token=${res?.rt}; Path=/; Domain=.webidemyyy.ir  Max-Age=64000000; SameSite=Lax; `
+    `x-refresh-token=${res?.rt}; Path=/; Domain=.webidemyyy.ir;  Max-Age=64000000; SameSite=Lax; `
   );
 
   return new Response("success login!", {
