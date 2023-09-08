d9e09fa7b8a7b235c052603f44022ac8571ad1a1
Alireza zamani <zamani802000@gmail.com>
2023-09-08 16:47:37 +0330

    fix project

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "api.webidemyyy.ir",
-         
-
-          },
-         
-        ],
-      },
 }
 
 module.exports = nextConfig
