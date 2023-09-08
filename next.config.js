5c770a6478c0c01e5203970032ab3faea189b0d7
Alireza zamani <zamani802000@gmail.com>
2023-09-08 17:34:33 +0330

    fix project

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: {
-      protocol: "https",
-      hostname: "api.webidemy.ir",
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.webidemyyy.ir",
+      },
+    ],
   },
 };
 
