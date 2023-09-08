c95a265305cd6b9e5dbe4372525b53b5bdeff590
Alireza zamani <zamani802000@gmail.com>
2023-09-08 16:30:03 +0330

    set domain

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,19 +2,18 @@ import { NextApiRequest } from "next";
 import { NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
   const res = await request.json();
-  
+
   const headers = new Headers();
-  console.log(res?.at,res?.rt);
-  
- 
+  console.log(res?.at, res?.rt);
+
   headers.append(
-    "Set-Cookie",`x-access-token=${res?.at}; Path=/; HttpOnly;  Max-Age=3000000  SameSite=Lax;`
+    "Set-Cookie",
+    `x-access-token=${res?.at}; Path=/; HttpOnly;  Max-Age=64000000 Domain=.webidemyyy.ir  SameSite=Lax;`
   );
   headers.append(
-    "Set-Cookie",`x-refresh-token=${res?.rt}; Path=/;  Max-Age=64000000; SameSite=Lax; `
+    "Set-Cookie",
+    `x-refresh-token=${res?.rt}; Path=/; Domain=.webidemyyy.ir  Max-Age=64000000; SameSite=Lax; `
   );
-
-
 
   return new Response("success login!", {
     status: 200,
